eb6ec3193a2f58fe1339bd4ab148b56ceeb035aa
Micaela Bray <micaela.bray@gmail.com>
2019-05-20 16:03:18 -0500

    fixes issue where current number did not update when photo was selected.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,22 @@
-var targetNumber = Math.floor(Math.random() * 120);
-var counterScore = 0;
-// var numberOptions = Math.floor(Math.random() *12);
-var cowOneValue;
-var cowOneImage = document.getElementsByClassName(".cowOneImage");
-var cowTwoValue;
-var cowTwoImage = document.getElementsByClassName(".cowTwoImage");
-var cowThreeValue;
-var cowThreeImage = document.getElementsByClassName(".cowThreeImage");
-var cowFourValue;
-var cowFourImage = document.getElementsByClassName(".cowFourImage");
-var wins = 0;
-var losses = 0;
+$(document).ready(function(){
 
-$("#targetNumber").text(targetNumber);
+    var targetNumber = Math.floor(Math.random() * 120);
+    var counterScore = 0;
+    var numberOptions = Math.floor(Math.random() *12);
+    var cowOneValue;
+    var cowOneImage = $(".cowOneImage");
+    var cowTwoValue;
+    var cowTwoImage = $(".cowTwoImage");
+    var cowThreeValue;
+    var cowThreeImage = $(".cowThreeImage");
+    var cowFourValue;
+    var cowFourImage = $(".cowFourImage");
+    var wins = 0;
+    var losses = 0;
 
-// function id(){
-//     var cowOneImage = document.getElementsByClassName(".cowOneImage")
-// }
+    $("#targetNumber").text(targetNumber);
 
-function reset() {
+    //Gives randomized value to each image
     cowOneValue=Math.floor(Math.random() *12) + 1;
         console.log(cowOneValue);
     cowTwoValue=Math.floor(Math.random() *12) + 1;
@@ -27,71 +25,74 @@ function reset() {
         console.log(cowThreeValue);
     cowFourValue=Math.floor(Math.random() *12) + 1;
         console.log(cowFourValue);
+    
+        
+    function reset() {    
+        cowOneImage.attr("data-cowOneValue", cowOneValue);
+        cowTwoImage.attr("data-cowTwoValue", cowTwoValue);
+        cowThreeImage.attr("data-cowThreeValue", cowThreeValue);
+        cowFourImage.attr("data-cowFourValue", cowFourValue);
+    };
 
-    // update();
-    // counterScore=0;
 
-    cowOneImage.attr("data-cowOneValue", cowOneValue);
-    cowTwoImage.attr("data-cowTwoValue", cowTwoValue);
-    cowThreeImage.attr("data-cowThreeValue", cowThreeValue);
-    cowFourImage.attr("data-cowFourValue", cowFourValue);
-};
+    $(".cowOneImage").on("click", function(){
+        counterScore += cowOneValue;
+        $("#currentNumber").text(counterScore);
+        if (counterScore === targetNumber){
+            wins ++;
+            reset();
+        }
+        else if (counterScore >= targetNumber){
+            losses++;
+            reset();
+        }
+    });
 
+    $(".cowTwoImage").on("click", function(){
+        counterScore += cowTwoValue;
+        $("#currentNumber").text(counterScore);
+        if (counterScore === targetNumber){
+            wins ++;
+            reset();
+        }
+        else if (counterScore >= targetNumber){
+            losses++;
+            reset();
+        }
+    });
 
-$(".cowOneImage").on("click", function(){
-    counterScore += cowOneValue;
-    $("#currentNumber").text(counterScore);
-    if (counterScore === targetNumber){
-        wins ++;
-        reset();
+    $(".cowThreeImage").on("click", function(){
+        counterScore += cowThreeValue;
+        $("#currentNumber").text(counterScore);
+        if (counterScore === targetNumber){
+            wins ++;
+            reset();
+        }
+        else if (counterScore >= targetNumber){
+            losses++;
+            reset();
+        }
+    });
+
+    $(".cowFourImage").on("click", function(){
+        counterScore += cowFourValue;
+        $("#currentNumber").text(counterScore);
+        if (counterScore === targetNumber){
+            wins ++;
+            reset();
+        }
+        else if (counterScore >= targetNumber){
+            losses++;
+            reset();
+        }
+    });
+
+    console.log("this is the counterScore " + counterScore);
+
+    function update () {
+        $("#currentNumber").text(counterScore);
+        $("#wins").text(wins);
+        $("#losses").text(losses);
     }
-    else if (counterScore >= targetNumber){
-        losses++;
-        reset();
-    }
+
 });
-
-$(".cowTwoImage").on("click", function(){
-    counterScore += cowTwoValue;
-    $("#currentNumber").text(counterScore);
-    if (counterScore === targetNumber){
-        wins ++;
-        reset();
-    }
-    else if (counterScore >= targetNumber){
-        losses++;
-        reset();
-    }
-});
-
-$(".cowThreeImage").on("click", function(){
-    counterScore += cowThreeValue;
-    $("#currentNumber").text(counterScore);
-    if (counterScore === targetNumber){
-        wins ++;
-        reset();
-    }
-    else if (counterScore >= targetNumber){
-        losses++;
-        reset();
-    }
-});
-
-$(".cowFourImage").on("click", function(){
-    counterScore += cowFourValue;
-    $("#currentNumber").text(counterScore);
-    if (counterScore === targetNumber){
-        wins ++;
-        reset();
-    }
-    else if (counterScore >= targetNumber){
-        losses++;
-        reset();
-    }
-});
-
-function update () {
-    $("#currentNumber").text(counterScore);
-    $("#wins").text(wins);
-    $("#losses").text(losses);
-}
